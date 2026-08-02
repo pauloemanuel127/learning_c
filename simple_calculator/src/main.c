72c620d042c7c0973a6dd4b8b909c0e4c5bb3058
pauloemanuel127 <pauloemanuel127@gmail.com>
2026-08-02 19:52:56 -0300

    Revert "made the loop"
    This reverts commit 930a2f047b328e2a1d4ad49c0b8c20311c03f4b3.

--- simple_calculator/src/main.c
+++ simple_calculator/src/main.c
@@ -5,9 +5,7 @@
  * @brief Arquivo principal (main), responsavel por aplicar a logica da calculadora, e trazer a interação entre o usuario e o codigo
  * 
  * @note Aqui foi o toque final do projeto, onde tive que me adaptar a linguagem de C para o uso das estruturas condicionais,
- * no futuro pretendo colocar um loop para manter a calculadora em funcionamento// implementada com sucesso!!.
- * 
- * @note Acho que no momento não imagino nenhuma mudança para esse mini projeto, por se tratar de uma simples adaptação acho que manterei assim
+ * no futuro pretendo colocar um loop para manter a calculadora em funcionamento.
  */
 
 #include <stdio.h>
@@ -17,7 +15,6 @@
  * @brief Função principal (main), responsavel pelo funcionamento do programa
  * 
  * Inicia a calculadora, e é responsavel pela identificação e o uso das funções para os calculos.
- * Funciona infinitamente por meio do loop while.
  * 
  * @return Retorna 0 indicando que o programa funcionou.
  */
@@ -25,60 +22,41 @@
 int main () {
 
     double a, b, result;
-    char op, option;
+    char op;
 
+    printf("Bem vindo a calculadora, digite sua operação de maneira algebrica como no exemplo: a + b."
+        "\nPara soma usamos o operador '+', para subtração '-', para multiplicação temos '*' e para divisão '/'\n");
+    scanf("%lf %c %lf", &a, &op, &b);
 
-    while (1) {
+    if (op == '+') {
 
-        printf("Bem vindo a calculadora, digite sua operação de maneira algebrica como no exemplo: a + b."
-            "\nPara soma usamos o operador '+', para subtração '-', para multiplicação temos '*' e para divisão '/'\n");
-        scanf("%lf %c %lf", &a, &op, &b);
-
-        if (op == '+') {
-
-            result = soma(a, b);
-            printf("A soma de %.2f e %.2f foi igual à %.2f\n", a, b, result);
-
-        }
-
-        else if (op == '-') {
-
-            result = subtracao(a, b);
-            printf("A subtração de %.2f e %.2f foi igual à %.2f\n", a, b, result);
-
-        }
-
-        else if (op == '*') {
-
-            result = multiplicacao(a, b);
-            printf("A multiplicação de %.2f e %.2f foi igual à %.2f\n", a, b, result);
-
-        }
-
-        else if (op == '/') {
-
-            result = divisao(a, b);
-            printf("A divisão de %.2f e %.2f foi igual a %.2f\n", a, b, result);
-
-        }
-
-        else {
-
-            printf("Operador invalido "
-                "\n tente rodar o programa novamente para poder realizar sua operação\n");
-
-        }
-
-        printf("Deseja continuar na calculadora? s/n\n");
-        scanf(" %c", &option);
-
-        if (option == 'n') {    
-
-            printf("Obrigado por usar a calculadora!!!");
-            break;
-        }
-
-
+        result = soma(a, b);
+        printf("A soma de %.2f e %.2f foi igual à %.2f\n", a, b, result);
     }
+
+    else if (op == '-') {
+
+        result = subtracao(a, b);
+        printf("A subtração de %.2f e %.2f foi igual à %.2f\n", a, b, result);
+    }
+
+    else if (op == '*') {
+
+        result = multiplicacao(a, b);
+        printf("A multiplicação de %.2f e %.2f foi igual à %.2f\n", a, b, result);
+    }
+
+    else if (op == '/') {
+
+        result = divisao(a, b);
+        printf("A divisão de %.2f e %.2f foi igual a %.2f\n", a, b, result);
+    }
+
+    else {
+
+        printf("Operador invalido "
+            "\n tente rodar o programa novamente para poder realizar sua operação\n");
+    }
+
     return 0;
 }
