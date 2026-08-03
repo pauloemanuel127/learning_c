f2ed3766284de0190fd7bdcc58ef906e1a95aa55
pauloemanuel127 <pauloemanuel127@gmail.com>
2026-08-03 20:47:37 -0300

    added bc1087

---
+++ beecrowd_C/bc1087.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    int x1, x2, y1, y2, abs_x, abs_y;
+
+    while (1) {
+        
+        scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+
+        int X[3] = {x1, y1};
+        int Y[3] = {x2, y2};
+
+        abs_x = x1 - x2;
+        abs_y = y1 - y2;
+
+        if (x1 == 0 && x2 == 0 && y1 == 0 && y2 == 0) {
+            break;
+        }
+
+        if(X[0] == Y[0] && X[1] == Y[1] && X[2] == Y[2]) {
+
+            printf("0\n");
+        }
+
+        else if(x1 == x2 || y1 == y2 || fabs(abs_x) == fabs(abs_y)) {
+
+            printf("1\n");
+        }
+
+        else {
+
+            printf("2\n");
+        }
+
+    }
+
+
+}
