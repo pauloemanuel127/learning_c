274d62dd561f57ab287b63cf685cac6c1c21c725
pauloemanuel127 <pauloemanuel127@gmail.com>
2026-08-03 21:03:30 -0300

    added bc1329

---
+++ beecrowd_C/bc1329.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+
+    int n, jogadas[10001];
+
+    while (1) {
+
+        int mary = 0;
+        int john = 0;
+
+        scanf("%d", &n);
+
+        if (n == 0) {
+
+            break;
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            scanf("%d", &jogadas[i]);
+        }
+
+        for (int j = 0; j < n; j++) {
+
+            if (jogadas[j] == 0) {
+
+                mary += 1;
+            }
+
+            else if (jogadas[j] == 1) {
+
+                john += 1;
+            }
+        }
+
+        printf("Mary won %d times and John won %d times\n", mary, john);
+    
+    }
+}   
