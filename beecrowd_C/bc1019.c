b5450359a776881dbf6feff72d96af2d10b6b938
pauloemanuel127 <pauloemanuel127@gmail.com>
2026-08-14 18:14:30 -0300

    add bc1019.c

---
+++ beecrowd_C/bc1019.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+
+  int N;
+  int h = 0, m = 0;
+
+  scanf("%d", &N);
+
+  while (N > 60) {
+
+    if (N > 3600) {
+
+      h += 1;
+      N -= 3600;
+
+    }
+
+    else if (N > 60) {
+
+      m += 1;
+      N -= 60;
+    }
+  }
+
+  printf("%d:%d:%d\n", h, m, N);
+
+  return 0;
+}
