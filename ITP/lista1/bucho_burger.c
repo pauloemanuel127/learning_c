67ba2be44b943219a82ee332118fbbc0a734db58
pauloemanuel127 <pauloemanuel127@gmail.com>
2026-08-18 00:55:06 -0300

    add one more exercise

---
+++ ITP/lista1/bucho_burger.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main() {
+
+    int item, valor, troco;
+
+    scanf("%d %d", &item, &valor);
+
+    switch (item) {
+
+        case 1:
+
+            troco =  valor - 12;
+            break;
+        
+        case 2:
+
+            troco = valor - 23;
+            break;
+        
+        case 3:
+
+            troco = valor - 31;
+            break;
+        
+        case 4:
+
+            troco = valor - 28;
+            break;
+        
+        case 5:
+
+            troco = valor - 15;
+            break;
+    
+    }
+
+    if (troco > 0) {
+
+        printf("Troco = %d reais", troco);
+    
+    }
+
+    else if (troco < 0) {
+
+        troco = troco * -1;
+        
+        printf("Saldo insuficiente! Falta %d reais", troco);
+    }
+    
+    else {
+
+        printf("Deu certim!");
+    }
+
+}
