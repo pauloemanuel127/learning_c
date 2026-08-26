78943e9f7da206df2fe7a1a0eddf487f0730f832
pauloemanuel127 <pauloemanuel127@gmail.com>
2026-08-26 20:47:38 -0300

    I decide to go with a more simple way

--- ITP/lista2/medias.c
+++ ITP/lista2/medias.c
@@ -1,64 +1,32 @@
 #include <stdio.h>
 #include <math.h>
 
-double calc_med(int arr[10], int type);
-double calc_mederr(double med1, double med2, int type);
+int main() {
 
-int main(void) {
-
-    int nums[10];
+    int input;
+    double arit_med = 0, geo_med = 1, harm_med = 0;
+    double err_med, geo_err, harm_err;
 
     for (int i = 0; i < 10; i++) {
-
-        scanf("%d", &nums[i]);
-    }
-
-    double arit_med = calc_med(nums, 0);
-    double geo_med = calc_med(nums, 1);
-    double harm_med = calc_med(nums, 2);
-
-    double harm_err = ();
-    double geo_err = ();
-
-}
-
-double calc_med(int arr[10], int type){
-
-    double ret = if (type == 2) 10 else 0;
-    double aux;
-
-    for (int i = 0; i < 10; i++){
-        
-        if (type == 2){
-            aux += (1 / arr[i]);
-        }
-        else ret += arr[i];
-    }
-
-    switch (type){
-
-        case 0: //ARITHMETRIC
-
-            ret = ret/10;
-            break;
-
-        case 1: //GEOMETRIC
-
-            ret = pow(ret, 0.1);
-            break;
-        
-        case 2: //HARMONIC
-            
-            ret /= aux;
-            break;
-
-    }
-
-    return ret;
-
-}
-
-double calc_mederr(double med1, double med2, int type) {
-
     
+        scanf("%d", &input);
+        arit_med += input;
+        geo_med *= input;
+        harm_med += 1/(double)input; //double para gerar um valor decimal
+
+    }
+
+    arit_med = arit_med/10;
+    geo_med = pow(geo_med, 0.1);
+    harm_med = 10/harm_med;
+
+    geo_err = (geo_med - arit_med)/arit_med;
+    harm_err = (harm_med - arit_med)/arit_med;
+    err_med = ((geo_err + harm_err)/2)*100;
+
+    printf("Média aritmética é %.2f\n", arit_med);
+    printf("Média harmônica é %.2f\n", harm_med);
+    printf("Média geométrica é %.2f\n", geo_med);
+    printf("Erro médio é %.2f %\n ", err_med); 
+
 }
