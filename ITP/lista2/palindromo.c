8b87695f7ef48d7b57acc16517c3f549d5a1d48c
pauloemanuel127 <pauloemanuel127@gmail.com>
2026-08-26 22:37:41 -0300

    fixed some bugs

--- ITP/lista2/palindromo.c
+++ ITP/lista2/palindromo.c
@@ -2,17 +2,17 @@
 
 int main(void) {
 
-    int val, palindromo, original, digito;
+    int val, palindromo = 0, original, digito;
 
     scanf("%d", &val);
 
-    original = val;
+    original = val; //Clone do valor de entrada para ser utilizado na comparação
 
     while (val) {
 
-        digito = val % 10;
-        palindromo = (palindromo * 10) + digito;
-        val /= 10;
+        digito = val % 10; //Salva o valor da unidade do numero
+        palindromo = (palindromo * 10) + digito; //Multiplica o valor anterior por 10 para depois adicionar o proximo digito, montando assim o palindromo
+        val /= 10; //Remove a unidade do numero pegando apenas a divisão sem o resto
     }
 
     if (palindromo == original) {
