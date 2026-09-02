45ce93349a888b90d88b90aa177a7d897dc20768
pauloemanuel127 <pauloemanuel127@gmail.com>
2026-09-02 19:24:43 -0300

    fixed colegas.c

--- ITP/lista3/colegas.c
+++ ITP/lista3/colegas.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int D(int num);
+int D(int num); //função que soma os divisores
 
-int abs(int val);
+int abs(int val); //função modular
 
-void colegas(int a, int b);
+void colegas(int a, int b); //função que verifica se são colegas
 
 int main(void) {
 
@@ -17,11 +17,11 @@ int main(void) {
 
 void colegas(int a, int b) {
 
-    if (abs(D(a) - b) <= 2 && abs(D(b) - a) <=2) {
-        printf("S");
+    if (abs(D(a) - b) <= 2 && abs(D(b) - a) <= 2) { //verificação
+        printf("S\n");
     }
     else {
-        printf("N");
+        printf("N\n");
     }
 }
 
@@ -37,5 +37,17 @@ int abs(int val) {
 
 int D(int num) {
 
-    
+    int total = 1;
+
+    for (int i = 2; i * i <= num; i++) { //iteração pelos possiveis valores divisores
+        if (num % i == 0) { //teste para verificar se é mesmo divisor
+            total += i;
+
+            if (i * i != num) { //teste para ver o valor complementar de i, para diminuir a complexidade algoritmica
+                total += num / i;
+            }
+        }
+    }
+
+    return total;
 }
