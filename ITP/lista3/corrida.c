b714d16f7e7e401e8473000b27602cc5cbf0afe8
pauloemanuel127 <pauloemanuel127@gmail.com>
2026-09-03 18:53:56 -0300

    the last one of lista3

--- ITP/lista3/corrida.c
+++ ITP/lista3/corrida.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+void alcancar(int x, int v1, int v2);
+
 int main(void) {
+
+    int dist, v1, v2;
+
+    scanf("%d %d %d", &dist, &v1, &v2);
+    alcancar(dist, v1, v2);
+
     return 0;
+}
+
+void alcancar(int x, int v1, int v2) {
+
+    if (v2 >= v1) {
+        printf("impossivel\n");
+        return;
+    }
+
+    printf("%ds\n", x / (v1 - v2));
 }
