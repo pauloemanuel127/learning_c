36516da28a41edb211abe41403150255c8a2dc26
pauloemanuel127 <pauloemanuel127@gmail.com>
2026-09-13 00:44:48 -0300

    add comments

--- ITP/lista4/search.c
+++ ITP/lista4/search.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-#include <string.h>
+#include <string.h> //lib string possui uma função para comparação de strings e vetores
 
-#define tam 8
+#define tam 8 //define o valor da variavel tam como constante para evitar a repetição
 
 int main(void) {
 
-    char frutas[tam][30];
-    for (int i = 0; i < tam; i++) {
+    char frutas[tam][30]; //vetor 2d que armazena as strings
+    for (int i = 0; i < tam; i++) { //iterador para armazenar as strings
         scanf("%29s", frutas[i]);
     }
 
     char alvo[30];
     scanf("%29s", alvo);
 
-    for (int i = 0; i < tam; i++) {
+    for (int i = 0; i < tam; i++) { //iterador para verificar a igualdade e gerar o retorno
         if (strcmp(frutas[i],alvo) == 0) {
             printf("encontrei\n");
         }
