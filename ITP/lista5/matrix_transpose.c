4333d93a2bff631071d62ca5b71180373cb25bd9
pauloemanuel127 <pauloemanuel127@gmail.com>
2026-09-15 21:51:23 -0300

    new comments in matrix_transpose.c

--- ITP/lista5/matrix_transpose.c
+++ ITP/lista5/matrix_transpose.c
@@ -6,7 +6,7 @@ int main(void) {
     int m,n;
     scanf("%d %d", &m, &n);
 
-    int a[m][n];
+    int a[m][n]; //matriz original MxN
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             scanf("%d", &a[i][j]);
@@ -17,8 +17,8 @@ int main(void) {
     transposta(m, n, a, b);
 
     printf("Transposta\n");
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++) { //print inverte n e m, pois como transposta ela se torna uma matriz NxM
+        for (int j = 0; j < m; j++) {
             printf("%d ", b[i][j]);
         }
         printf("\n");
@@ -30,7 +30,7 @@ int main(void) {
 void transposta(int m, int n, int matriz[m][n], int result[n][m]) {
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-            result[j][i] = matriz[i][j];
+            result[j][i] = matriz[i][j]; //inverte j e i no resultado para fazer a transposta
         }
     }
 }
