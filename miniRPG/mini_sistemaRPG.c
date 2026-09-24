fd2a3b6197bdc933075c52ebbbdd317af0592344
pauloemanuel127 <pauloemanuel127@gmail.com>
2026-09-24 19:56:23 -0300

    second func made

--- miniRPG/mini_sistemaRPG.c
+++ miniRPG/mini_sistemaRPG.c
@@ -11,3 +11,42 @@ Personagem make_char(char name[30], Classes classe, Elementos type, int level, i
 
     return x;
 }
+
+void view_char(Personagem character) {
+    printf("O personagem %s, tem as seguintes carateristicas:\n", character.nome);
+
+    switch (character.classe) {
+        case GUERREIRO:
+            printf("classe: Guerreiro\n");
+            break;
+        case MAGO:
+            printf("classe: Mago\n");
+            break;
+        case LADINO:
+            printf("classe: Ladino\n");
+            break;
+        case ARQUEIRO:
+            printf("classe: Arqueiro\n");
+            break;
+    }
+
+    switch (character.tipo) {
+        case AGUA:
+            printf("tem magia do elemento: Agua\n");
+            break;
+        case TERRA:
+            printf("tem magia do elemento: Terra\n");
+            break;
+        case FOGO:
+            printf("tem magia do elemento: Fogo\n");
+            break;
+        case AR:
+            printf("tem magia do elemento: Ar\n");
+            break;
+    }
+
+    printf("Atualmente está nivel: %d\n", character.nivel);
+    printf("possui %d de vida\n", character.vida);
+
+    return;
+}
